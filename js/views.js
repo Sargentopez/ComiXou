7dedb55b47fc307bb9e08498b5baebbd12b57ecd
Sargentopez <albertobicho@gmail.com>
2026-05-28 18:49:38 +0200

    c

--- js/views.js
+++ js/views.js
@@ -50,7 +50,7 @@ Router.register('home', {
       </div>
     <main class="home-list" id="comicsGrid">
     </main>
-    <footer class="app-version">v27.61</footer>
+    <footer class="app-version">v27.66</footer>
   `,
   init: () => { HomeView_init(); },
   destroy: () => { if (window._homeStoreCleanup) { window._homeStoreCleanup(); window._homeStoreCleanup = null; } }
